a45c38b13955cd3562918b04949dd90cb419cfeb
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-08-13 13:51:35 +0200

    Rewrite general code to tide up

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
-app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://qknts.netlify.app"],
-    credentials: true,
-  })
-);
 app.use(morgan("dev"));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to the API");
-});
+app.use(cookieParser());
 
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const TOKEN_SECRET = "some secret key";
