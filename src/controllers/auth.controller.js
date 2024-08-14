7ffba81c075f25088b1c3a65239d4f8843b92cdd
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-08-14 09:08:53 +0200

    Fix auth controller and middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,6 +7,12 @@ import { createAccessToken } from "../libs/jwt.js";
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
+    const userFound = await User.findOne({ email });
+
+    if (userFound) {
+      return res.status(400).json(["User already exists"]);
+    }
+
     const passwordHash = await bcrypt.hash(password, 10);
 
     const newUser = new User({
