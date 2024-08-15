f6ea59abbf9439351fc11dcb696a41c5e5bfe441
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-08-15 18:55:25 +0200

    Fix cors

--- src/app.js
+++ src/app.js
@@ -5,11 +5,19 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 
+const allowedOrigins = ["http://localhost:5173", "https://qknts.netlify.app"];
+
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
